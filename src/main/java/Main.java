2a4265c7a22f2413faf22feee81f2622899fab52
teemvat <teemu.vataja@metropolia.fi>
2025-03-11 18:26:41 +0200

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,6 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    // comment for testing webhook
 
     @Override
     public void start(Stage stage) throws Exception {
